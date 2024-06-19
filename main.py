03f709674c2feebf2328f344470a8feeb106d589
mcgue <mc8@protonmail.com>
2024-06-19 22:39:32 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+# Draw a square with pygame
+# Import and initialize the pygame library
 import pygame
 
-print ("Test")
+def main():
+    pygame.init()
+
+    # Set up the drawing window
+    display = pygame.display.set_mode([100, 100])
+
+
+    # Run until the user asks to quit
+    #running = True
+    # while running:
+        # Close the window is user clicked close button
+        # for event in pygame.event.get():
+        #    if event.type == pygame.QUIT:
+        #        running = False
+    
+  # Set colors
+    WHITE=(255,255,255)
+    BLUE=(0,0,255)
+    
+  # Fill the background with white
+    display.fill(WHITE)
+
+    # Draw a rectangle
+    pygame.draw.rect(display, BLUE, (20,20,20,20))
+
+    # Draw it
+    pygame.display.update()
+
+# Run it
+main()
+
+# Quit the game
+pygame.quit()
